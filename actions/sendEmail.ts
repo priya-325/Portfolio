07095424a6c9632c63ba0462c14fb0d38b8d2a9a
Priyanka <priyanka.e.03.12@gmail.com>
2024-06-03 23:27:20 +0100

    api edited

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,8 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_X6vakj5X_Cu7Jmvv1FXDVycDNzWj3K7oU");
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
